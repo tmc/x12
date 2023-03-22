a0bc2eb72504b737332a56c0b449be895fe22d78
Travis Cline <travis.cline@gmail.com>
2023-03-21 20:29:49 -0700

    x12: improve organization a bit

--- types.go
+++ types.go
@@ -1,47 +1,99 @@
-// Based on http://www.wpc-edi.com/reference/repository/006020CC.PDF
-
 package x12
 
-type ID string
+// X12Document is the root element of an X12 document.
+type X12Document struct {
+	Interchange *Interchange
+}
 
-// AN represents an alphanumeric field
-type AN string
+// Interchange is the envelope for an X12 interchange.
+type Interchange struct {
+	Header         *ISA
+	FunctionGroups []*FunctionGroup
+	Trailer        *IEA
+}
 
-// SegmentType represents the type of segment in an X12 document.
-type SegmentType string
+// ISA is the Interchange Control Headera.
+type ISA struct {
+	AuthorizationInfoQualifier     string
+	AuthorizationInformation       string
+	SecurityInfoQualifier          string
+	SecurityInfo                   string
+	InterchangeSenderIDQualifier   string
+	InterchangeSenderID            string
+	InterchangeReceiverIDQualifier string
+	InterchangeReceiverID          string
+	InterchangeDate                string
+	InterchangeTime                string
+	InterchangeControlStandardsID  string
+	InterchangeControlVersion      string
+	InterchangeControlNumber       string
+	AcknowledgmentRequested        string
+	UsageIndicator                 string
+	ComponentElementSeparator      string
+}
 
-const (
-	// ISA Interchange Control Header Segment
-	SegmentTypeISA SegmentType = "ISA"
-	// GS  Function Group Header Segment
-	SegmentTypeGS SegmentType = "GS"
-	// ST  Transaction Set Header Segment
-	SegmentTypeST SegmentType = "ST"
-	// SE  Transaction Set Trailer Segment
-	SegmentTypeSE SegmentType = "SE"
-	// GE  Function Group Trailer Segment
-	SegmentTypeGE SegmentType = "GE"
-	// IEA Interchange Control Trailer Segment
-	SegmentTypeIEA SegmentType = "IEA"
-)
+// IEA is the Interchange Control Trailer.
+type IEA struct {
+	NumberOfIncludedFunctionalGroups string
+	InterchangeControlNumber         string
+}
 
-const isaLength = 106
+// FunctionGroup is a group of transactions.
+type FunctionGroup struct {
+	Header       *GS
+	Transactions []*Transaction
+	Trailer      *GE
+}
 
-type ISASegment struct {
-	AuthorizationInformationQualifier ID   // ISA01 Authorization Information Qualifier - Code identifying the type of information in the Authorization Information.
-	AuthorizationInformation          AN   // ISA02 Authorization Information - Information used for additional identification or authorization of the interchange sender or the data in the interchange; the type of information is set by the Authorization Information Qualifier (I01).
-	SecurityInformationQualifier      ID   // ISA03 Security Information Qualifier - Code identifying the type of information in the Security Information
-	SecurityInformation               AN   // ISA04 Security Information - This is used for identifying the security information about the interchange sender or the data in the interchange; the type of information is set by the Security Information Qualifier (I03).
-	InterchangeSenderIDQualifier      ID   // ISA05 Interchange ID Qualifier - Code indicating the system/method of code structure used to designate the sender or receiver ID element being qualified. This ID qualifies the Sender in ISA06.
-	InterchangeSenderID               ID   // ISA06 Interchange Sender ID - Identification code published by the sender for other parties to use as the receiver ID to route data to them; the sender always codes this value in the sender ID element.
-	InterchangeReceiverIDQualifier    ID   // ISA07 Interchange ID Qualifier - Code indicating the system/method of code structure used to designate the sender or receiver ID element being qualified.
-	InterchangeReceiverID             ID   // ISA08 Interchange Receiver ID - Identification code published by the receiver of the data; When sending, it is used by the sender as their sending ID, thus other parties sending to them will use this as a receiving ID to route data to them.
-	InterchangeDate                   ID   // ISA09 Interchange Date - Date of the interchange. The date format is YYMMDD.
-	InterchangeTime                   ID   // ISA10 Interchange Time - Time of the interchange. The time format is HHMM.
-	RepetitionSeparator               byte // ISA11 Repetition Separator - The repetition separator is a delimiter and not a data element; this field provides the delimiter used to separate repeated occurrences of a simple data element or a composite data structure; this value must be different than the data element separator, component element separator, and the segment terminator.
-	InterchangeControlVersionNumber   ID   // ISA12 Interchange Control Version Number - Code specifying the version number of the interchange control segments.
-	InterchangeControlNumber          int  // ISA13 Interchange Control Number - A control number assigned by the interchange sender.
-	AcknowledgementRequested          bool // ISA14 Acknowledgement Requested - Code indicating sender’s request for an interchange acknowledgment.
-	TestIndicator                     bool // ISA15 Test Indicator - Code indicating whether data enclosed by this interchange envelope is test, production or information. P - Production, T - Test.
-	ComponentElementSeparator         ID   // ISA16 Component Element Separator - The component element separator is a delimiter and not a data element; this field provides the delimiter used to separate component data elements within a composite data structure; this value must be different than the data element separator and the segment terminator.
+// GS is the Functional Group Header.
+type GS struct {
+	FunctionalIDCode         string
+	ApplicationSenderCode    string
+	ApplicationReceiverCode  string
+	Date                     string
+	Time                     string
+	GroupControlNumber       string
+	ResponsibleAgencyCode    string
+	VersionReleaseIndustryID string
+}
+
+// GE is the Functional Group Trailer.
+type GE struct {
+	NumberOfIncludedTransactionSets string
+	GroupControlNumber              string
+}
+
+// Transaction is a single transaction.
+type Transaction struct {
+	Header   *ST
+	Segments []Segment
+	Trailer  *SE
+}
+
+// ST is the Transaction Set Header.
+type ST struct {
+	TransactionSetIDCode              string
+	TransactionSetControlNumber       string
+	ImplementationConventionReference string
+}
+
+// SE is the Transaction Set Trailer.
+type SE struct {
+	NumberOfIncludedSegments    string
+	TransactionSetControlNumber string
+}
+
+// Segment is a single segment.
+// A segment is a single line of an X12 document.
+type Segment struct {
+	ID       string
+	Elements []Element
+}
+
+// Element is a single element.
+// An element is a single value in a segment.
+type Element struct {
+	ID         string
+	Value      string
+	Components []string `json:",omitempty"`
 }
--- x12.go
+++
@@ -1,5 +0,0 @@
-// Package x12 implements tools for dealing with ASC X12 EDI data.
-//
-// Based on version 006010
-// http://www.wpc-edi.com/reference/repository/006010cc.pdf
-package x12
